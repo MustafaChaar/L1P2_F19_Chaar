9c6e1dcb235ec3080962094cfd9a3dcfe9a5ed16
Mustafa Chaar <mustafa_abdulkareem@hotmail.com>
2019-12-02 13:40:14 +0200

    delete script

--- exec_1.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-echo "Please enter a sentence that includes the letter i"
-read foo
-var=$(echo $foo | tr "i" "a")
-var2=$(echo $var | tr "{a-z}" "{A-Z}")
-echo $var2
--- exec_2.sh
+++
@@ -1,5 +0,0 @@
-echo "Please enter your name"
-read name
-echo "Please enter an integer"
-read nbr
-touch ${name}_${nbr}
